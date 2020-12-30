70d88b96928351701d69679e4b3c293b9876a8ac
Lukas <lukas.rohde98@gmail.com>
2020-12-30 17:53:51 +0100

    fixed ignore + added reading .shp files

--- src/Matcher.py
+++ src/Matcher.py
@@ -1,25 +1,39 @@
-from OSMPythonTools.nominatim import Nominatim
-import pandas as pd
+import os
+
 import geopandas as geopd
+import pandas as pd
+from OSMPythonTools.nominatim import Nominatim
 
 nom = Nominatim()
 
+dataPath = os.path.dirname(__file__) + "/data/sensibleData"
+timetables = dataPath + "/timetables/rohdaten/stamm/bfkoord.txt"
+prognose = dataPath + "/prognose"
+vg = dataPath + "/vg250/vg250_kompakt_3112/VG250_F.shp"
 
-def get_data(path):
-    return open(path, "r", encoding="iso-8859-1")
+def handle_timetable():
+    def read_timetable(path):
+        return open(path, "r", encoding="iso-8859-1")
 
+    fahrplan_file = read_timetable(timetables)
+    fahrplan = fahrplan_file.readlines()
 
-file = get_data(
-    "/Users/lukas/Documents/Uni/7.Semester/Algo_Praktikum/sensibleData/timetables/Data/rohdaten/stamm/bfkoord.txt")
-lines = file.readlines()
+    for line in fahrplan:
+        lineSplit = line.split(' ')
+        lineSplit = [lineSplit[0], lineSplit[2], lineSplit[1]]
+        # 0 -> AreaCode
+        # 1 -> Longitude
+        # 2 -> Latitude
+        if lineSplit[0].startswith('0') or lineSplit[0].startswith('80'):
 
-for line in lines:
-    lineSplit = line.split(' ')
-    lineSplit = [lineSplit[0], lineSplit[2], lineSplit[1]]
+            location = nom.query(lineSplit[1], lineSplit[2], reverse=True, zoom=18)
 
-    if lineSplit[0].startswith('0') or lineSplit[0].startswith('80'):
-        location = nom.query(lineSplit[1], lineSplit[2], reverse=True, zoom=18)
+            print(location.areaId())
+            print(location.address())
+            print(lineSplit[0])
 
-        print(location.areaId())
-        print(location.address())
-        print(lineSplit[0])
+def handle_landkreise():
+    landkreise = geopd.read_file(vg)
+    print(landkreise.describe())
+
+handle_landkreise()
