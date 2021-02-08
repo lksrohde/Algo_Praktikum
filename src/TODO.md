d72e0a7847b24ce2e5ef000e96dbdaa8b4d0da9a
Lukas <lukas.rohde98@gmail.com>
2021-02-08 16:54:13 +0100

    Finished matching DB_Haltestellen and Kreise

--- src/TODO.md
+++ src/TODO.md
@@ -1,11 +1,9 @@
-# Erste Schritte
+# TODO
 
-- Zuordnung der Verkehrszellen aus der Verflechtungsprognose zu Stationen aus dem Fahrplan
-  - Amtliche Kreisschlüssel → Polygone
-  - Für jede Station des Fahrplans ermitteln, in welchem Kreis sie liegt
 - Aufteilung des Verkehrsaufkommens auf Stationen in den Kreisen
   - Für den Anfang: Fern- und Regionalverkehr zwischen Kreisen
 - Generierung von sinnvollen Abfahrts- oder Ankunftszeiten je nach Reisezweck
 - Ausgabe: Routing-Anfragen für MOTIS im JSON-Format
   - Vorwärtssuche: Startstation, Zielstation, gewünschte Abfahrtszeit
   - Rückwärtssuche: Startstation, Zielstation, gewünschte Ankunftszeit
+
--- src/fahrplan_matcher.py
+++ src/fahrplan_matcher.py
@@ -1,54 +1,117 @@
+import json
 import os
 
+import OSMPythonTools.nominatim
 import geopandas as geopd
-import geoplot as geoplt
-import geoplot.crs as gcrs
-import matplotlib.pyplot as plt
+import geopy
 import pandas as pd
-from OSMPythonTools.nominatim import Nominatim
-from shapely.geometry import Point, Polygon
+import shapely.geometry
 
-nom = Nominatim()
+nom = OSMPythonTools.nominatim.Nominatim()
+geopy_nom = geopy.Nominatim(user_agent="fahrplan_matcher.py")
 
-dataPath = os.path.dirname(__file__) + "/data/sensibleData"
-timetables = dataPath + "/timetables/rohdaten/stamm/bfkoord.txt"
-prognose = dataPath + "/prognose"
-vg = dataPath + "/vg250/vg250_kompakt_3112/VG250_F.shp"
+dataPath = os.path.dirname(__file__) + "/data/"
+timetables = dataPath + "sensibleData/timetables/Data/rohdaten/stamm"
+prognose = dataPath + "sensibleData/verkehrsverflechtungsprognose/PVMatrix_BVWP15_P2030/PVMatrix_BVWP15_P2030.csv"
+vg = dataPath + "sensibleData/vg250/vg250_kompakt_3112/VG250_F.shp"
 
-def handle_timetable():
-    def read_timetable(path):
-        return open(path, "r", encoding="iso-8859-1")
+verkehrszellen_mapping = dataPath + "sensibleData/verkehrsverflechtungsprognose/PVMatrix_BVWP15_P2030/Verkehrszellen_BVWP15.xlsx"
+json_haltestellen = timetables + "/newData/haltestellen"
+aggregated_nuts_regions = dataPath + "nuts.geojson/aggregated_nuts_regions.json"
 
-    fahrplan_file = read_timetable(timetables)
-    fahrplan = fahrplan_file.readlines()
 
-    for line in fahrplan:
+def read_timetable(path):
+    return open(path, "r", encoding="iso-8859-1")
+
+def truncate(f, n):
+    '''Truncates/pads a float f to n decimal places without rounding'''
+    s = '{}'.format(f)
+    if 'e' in s or 'E' in s:
+        return '{0:.{1}f}'.format(f, n)
+    i, p, d = s.partition('.')
+    return float('.'.join([i, (d+'0'*n)[:n]]))
+
+def match_kreis_haltestelle_simplified():
+    aggregated_nuts_json = open(aggregated_nuts_regions, "r")
+    aggregated_nuts_data = json.load(aggregated_nuts_json)
+
+    de_path = dataPath + "landkreise_simplify0.geojson"
+    de_geo_json = geopd.read_file(de_path)
+
+    nuts_path = "/Users/lukas/Documents/Uni/7.Semester/Algo_Praktikum/src/data/exact_nuts.geojson/NUTS_RG_01M_2010_4326.geojson"
+    nuts_geo_json = geopd.read_file(nuts_path)
+
+    # Koordinaten der Haltestellen
+    haltestellen_data = read_timetable(timetables + "/bfkoord.txt")
+
+    # Zuordnung Kreisname auf KKZ
+    excel_file = pd.ExcelFile(verkehrszellen_mapping)
+    prognose_map_data_de = pd.read_excel(excel_file, "ME1_D_AKS")
+    prognose_map_data_rest = pd.read_excel(excel_file, "ME1_Ausland")
+
+    output_file = open(json_haltestellen + "_mapped.json", "w")
+    levl_codes = {
+        "0": ["N", "FIN", "EE", "LV", "LT", "RU2", "RU1", "BY", "UA", "MD", "RO", "BG", "TR"
+            , "GR", "MK", "AL", "XK", "ME", "SRB", "BIH", "HR", "SL", "ES", "PT", "IRL"]
+    }
+    def handle_region(line_split):
+        lon = float(line_split[1])
+        lat = float(line_split[2])
+
+        # If not found anything use broader coords
+        for i in range(8,0, -1):
+            lon = truncate(lon, i)
+            lat = truncate(lat, i)
+
+            coords = shapely.geometry.Point(lat, lon)
+
+            print("Current Point: ", coords)
+            # Handle Germany
+            for i, kreis in enumerate(de_geo_json['geometry']):
+                if kreis.contains(coords):
+                    print(line_split[0], coords, de_geo_json['GEN'][i], de_geo_json['AGS'][i])
+                    return line_split[0], coords, de_geo_json['GEN'][i], de_geo_json['AGS'][i]
+            print("Deutschland gecheckt!")
+
+            #Handle Nuts
+            for i, nuts in enumerate(nuts_geo_json.get('geometry')):
+                if nuts.contains(coords):
+
+                    name = nuts_geo_json['NUTS_NAME'][i]
+                    id = nuts_geo_json['NUTS_ID'][i]
+                    code = nuts_geo_json["LEVL_CODE"][i]
+
+                    if not (id in levl_codes["0"]) and code == 0:
+                        continue
+
+                    print("gefunden", name, id)
+
+                    for j, prognose in enumerate(prognose_map_data_rest['Bezeichnung_ME1']):
+                        if name in prognose:
+                            print(line_split[0], coords, name, prognose_map_data_rest['Nr_ME1'][j], "Nuts")
+                            return line_split[0], coords, name, prognose_map_data_rest['Nr_ME1'][j]
+
+                    for aggregated in aggregated_nuts_data:
+                        if id in aggregated_nuts_data[aggregated]['regions']:
+                            print(line_split[0], coords, name, aggregated, "Agg. Nuts")
+                            return line_split[0], coords, name, aggregated
+
+            print("Going Broader")
+    # Generate Master File
+    # Data: DB_ID, Point, Region, KKZ/NUTS (VP_ID)
+    dict_file = {}
+    for line in haltestellen_data:
         line_split = line.split(' ')
+        # 0 -> AreaCode     # 1 -> Longitude    # 2 -> Latitude
         line_split = [line_split[0], line_split[2], line_split[1]]
-        # 0 -> AreaCode
-        # 1 -> Longitude
-        # 2 -> Latitude
+
+        # Bus und Bahn Verkehr
         if line_split[0].startswith('0') or line_split[0].startswith('80'):
 
-            location = nom.query(line_split[1], line_split[2], reverse=True, zoom=18)
-            point = Point(float(line_split[2]), float(line_split[1]))
-            get_verwaltungsbezirk(point)
-            #print(location.areaId())
-            print(location.address())
-            #print(line_split[0])
-
-def get_verwaltungsbezirk(haltestelle):
-    vw_bezirke = geopd.read_file(vg)
+            db_ip, point, region, vp_ip = handle_region(line_split)
+            dict_entry = {"DB_IP": db_ip, "POINT": {"lon": line_split[1], "lat": line_split[2]}, "REGION": region, "VP_IP": vp_ip}
+            dict_file[vp_ip] = dict_entry
     
-    temp0 = {'ARS': [vw_bezirke.get("ARS")[11151]], 'geometry': vw_bezirke.get("geometry")[11151]}
-    gdf = geopd.GeoDataFrame(temp0)
-    print(gdf)
-    #geoplt.polyplot(vw_bezirke)
-    gdf.plot()
-    plt.show()
-    vw_bezirke.plot()
-    plt.show()    
-    #print(haltestelle.within(vw_bezirke))   
+    output_file.write(dict_file.__str__().replace("'",'"'))
 
-#handle_timetable()
-get_verwaltungsbezirk(1)
+match_kreis_haltestelle_simplified()
